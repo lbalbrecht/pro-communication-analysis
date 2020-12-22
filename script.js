ca7895e1431482d90b6e7b1cb05ad1fa0ebc452a
JohnDJake <johndjake@gmail.com>
2020-12-22 10:46:58 -0800

    call the google natural language API via my server instead of the other APIs

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var submitButton = $("#submit-button");
 
 var sampleText = "First, the good news: The Moderna vaccine will likely be going into people's arms Monday, boosting the number of Americans who will start getting inoculated against Covid-19. Here is more text in that vein. I do not know why the first text didn't work. Here's a negative sentence, everyone hates you and you should die.";
 
-var queryURL = "https://sentim-api.herokuapp.com/api/v1/";
+/* var queryURL = "https://sentim-api.herokuapp.com/api/v1/";
 
 var entityURL = "https://api.dandelion.eu/datatxt/nex/v1/?lang=en&text=" + sampleText + "&token=fbcac4b6dc234d639b4e5d6c14191f7f";
 
@@ -21,26 +21,44 @@ $.ajax({
     data: JSON.stringify({
         text: sampleText
     })
-  }).then(function(response){
-      console.log(response);
+}).then(function (response) {
+    console.log(response);
 
-      console.log(response.sentences[0].sentiment.type);
+    console.log(response.sentences[0].sentiment.type);
 
-      for(i=0; i<response.sentences.length; i++){
-          if(response.sentences[i].sentiment.polarity<0){
-              console.log("You're a jerk")
-          }
-          else{
-              console.log("Nah you're good");
-          }
-      }
-    
-  })
+    for (i = 0; i < response.sentences.length; i++) {
+        if (response.sentences[i].sentiment.polarity < 0) {
+            console.log("You're a jerk")
+        }
+        else {
+            console.log("Nah you're good");
+        }
+    }
 
-  $.ajax({
-      url: entityURL,
-      method: "POST",
-      data: sampleText
-  }).then(function(response){
-      console.log(response);
-  })
+})
+
+$.ajax({
+    url: entityURL,
+    method: "POST",
+    data: sampleText
+}).then(function (response) {
+    console.log(response);
+}) */
+
+
+
+$(document).ready(function () {
+    $("#user-text").submit(function (event) {
+        // prevent default form behavior
+        event.preventDefault();
+        $.ajax({
+            // data is the text the user entered
+            data: $("#textarea1").val(),
+            // my server that handles the authenticated api request
+            url: "http://34.83.70.58:5000/",
+            method: "POST"
+        }).then(function (response) {
+            console.log(response);
+        })
+    })
+})

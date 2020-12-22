eda82a85964fb2ebd826b00a8fe669f894979fb0
RachelNS <rachelnelsonschille@gmail.com>
2020-12-22 10:36:12 -0800

    corrected submit button selector

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Grabs the "submit" button
-var submitButton = $("#submit");
+var submitButton = $("#submit-button");
 
 var sampleText = "First, the good news: The Moderna vaccine will likely be going into people's arms Monday, boosting the number of Americans who will start getting inoculated against Covid-19. Here is more text in that vein. I do not know why the first text didn't work. Here's a negative sentence, everyone hates you and you should die.";
 
